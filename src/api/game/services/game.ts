a5897c222dbb8e7790653836e51b2411801f0321
Eula Ludmila <eula.silva.teixeira@gmail.com>
2024-08-21 18:52:46 -0300

    feat: scrapper of data with jsdom

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,16 +1,41 @@
 /**
  * game service
  */
-
+import axios from 'axios'
+import { JSDOM } from 'jsdom'
 import { factories } from '@strapi/strapi';
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll('-', '_').toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/game/${gogSlug}`)
+  const dom = new JSDOM(body.data)
+  const raw_description = dom.window.document.querySelector('.description')
+  const description = raw_description.innerHTML
+  const short_description = raw_description.textContent.slice(0, 160)
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  )
+
+
+  return {
+    description,
+    short_description,
+    rating: ratingElement 
+    ? ratingElement.getAttribute("xlink:href")
+      .replace(/_/g, "")
+      .replace("#", "") 
+    : 'BR0'
+  }
+}
 
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
-    //Buscando categorias conforme o nome delas (filtro)
-    const cat = await strapi.service("api::category.category").find({
-      filters: {name: params.category}
-    });
-    
-    console.log(cat);
+    const gogApiUrl=`
+https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`
+
+    const { data: { products } } = await axios.get(gogApiUrl);
+    console.log(products[0].slug)
+    console.log(await getGameInfo(products[0].slug))
   }
 }));
