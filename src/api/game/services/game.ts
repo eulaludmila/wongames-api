ce8c20136eea4084e361dc17ae8a9991f4082e76
Eula Ludmila <eula.silva.teixeira@gmail.com>
2024-09-12 00:41:48 -0300

    feat: create many to many data

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -4,6 +4,13 @@
 import axios from 'axios'
 import { JSDOM } from 'jsdom'
 import { factories } from '@strapi/strapi';
+import slugify from 'slugify';
+
+const GAME_SERVICE = "api::game.game"; //
+const PUBLISHER_SERVICE = "api::publisher.publisher";
+const DEVELOPER_SERVICE = "api::developer.developer";
+const CATEGORIES_SERVICE = "api::category.category";
+const PLATFORM_SERVICE = "api::platform.platform";
 
 async function getGameInfo(slug) {
   const gogSlug = slug.replaceAll('-', '_').toLowerCase();
@@ -17,7 +24,6 @@ async function getGameInfo(slug) {
     ".age-restrictions__icon use"
   )
 
-
   return {
     description,
     short_description,
@@ -29,13 +35,82 @@ async function getGameInfo(slug) {
   }
 }
 
+//Função para validar se o dado já existe no strapi
+async function getByName(name, entityService) {
+  //buscando na entidade se existe o filtro "name"
+  const item = await strapi.service(entityService).find({
+    filters: {name},
+  })
+
+  //se não existir o item cadastrado retorna os resultados
+  return item.results.length > 0 ? item.results[0] : null
+}
+
+async function create(name, entityService) {
+  const item = await getByName(name, entityService)
+  if(!item){
+    //TO-DO usar o serviço do strapi, passando o serviço e usando o método create para cadastrar
+    await strapi.service(entityService).create({
+      data: {
+        name: name,
+        slug: slugify(name, { strict: true, lower: true })//retirar caracteres especiais e coloca em minusculo
+      }
+    })
+  }
+}
+
+//Função para realizar a criação dos dados
+async function createManyToManyData(products){
+  const developerSet = new Set();
+  const publishersSet = new Set();
+  const categoriesSet = new Set();
+  const plataformsSet = new Set();
+
+  products.forEach((product) => {
+    const { developers, publishers, genres, operatingSystems } = product;
+
+    genres?.forEach(({ name }) => {
+      categoriesSet.add(name)
+    })
+
+    operatingSystems?.forEach((item) => {
+      if(item){
+        plataformsSet.add(item)
+      }
+    })
+
+    developers?.forEach((item) => {
+      if(item){
+        developerSet.add(item)
+      }
+    })
+
+    publishers?.forEach((item) => {
+      if(item){
+        publishersSet.add(item)
+      }
+    })
+
+    const createCall = (set, entityName) => Array.from(set).map(async (name) => await create(name, entityName))
+
+    return Promise.all([
+      ...createCall(developerSet, DEVELOPER_SERVICE),
+      ...createCall(categoriesSet, CATEGORIES_SERVICE),
+      ...createCall(publishersSet, PUBLISHER_SERVICE),
+      ...createCall(plataformsSet, PLATFORM_SERVICE),
+    ])
+  })
+}
+
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
+    //buscando o catalogo de jogos no site Gog
     const gogApiUrl=`
 https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`
 
     const { data: { products } } = await axios.get(gogApiUrl);
-    console.log(products[0].slug)
-    console.log(await getGameInfo(products[0].slug))
+
+    //criar todos os dados necessário para o cadastro dos jogos
+    await createManyToManyData(products)
   }
 }));
