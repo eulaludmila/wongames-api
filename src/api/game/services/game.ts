e6e6d85f89b6fb8e27e519558f95fa6277390f17
Eula Ludmila <eula.silva.teixeira@gmail.com>
2024-09-17 18:34:06 -0300

    feat: creating game

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -5,6 +5,8 @@ import axios from 'axios'
 import { JSDOM } from 'jsdom'
 import { factories } from '@strapi/strapi';
 import slugify from 'slugify';
+import { get } from 'http';
+import developer from '../../developer/controllers/developer';
 
 const GAME_SERVICE = "api::game.game"; //
 const PUBLISHER_SERVICE = "api::publisher.publisher";
@@ -12,6 +14,7 @@ const DEVELOPER_SERVICE = "api::developer.developer";
 const CATEGORIES_SERVICE = "api::category.category";
 const PLATFORM_SERVICE = "api::platform.platform";
 
+//Busca as informações no site 'gogo' e retorna as informações necessárias para o cadastro
 async function getGameInfo(slug) {
   const gogSlug = slug.replaceAll('-', '_').toLowerCase();
 
@@ -102,15 +105,105 @@ async function createManyToManyData(products){
   })
 }
 
+//Precisa liberar o upload em settings no admin
+async function setImage({image, game, field = "cover"}) {
+  const { data } = await axios.get(image, { responseType: "arraybuffer" }); //requisição na url e salve em buffer a imagem
+  const buffer = Buffer.from(data, "base64");//salva o data na base64
+
+  const FormData = require("form-data");//para salvar a imagem
+
+  const formData:any = new FormData();//crio um novo form-data
+
+  //passo rodas as informações da imagem
+  formData.append("refId", game.id);
+  formData.append("ref", `${GAME_SERVICE}`);
+  formData.append("field", field);
+  formData.append("files", buffer, { filename: `${game.slug}.jpg`});
+
+  console.info(`Uploading ${field} image: ${game.slug}.jpg`);
+
+  //realizo a requisição para salvar a imagem
+  await axios({
+    method: "POST",
+    url: `http://localhost:1338/api/upload/`,
+    data: formData,
+    headers: {
+      "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+    },
+  })
+}
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, GAME_SERVICE)
+
+      if(!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.service(`${GAME_SERVICE}`).create({
+          data: {
+            name: product.title,
+            slug: product.slug,
+            price: product.price.finalMoney.amount,
+            release_date: new Date(product.releaseDate),
+            categories: await Promise.all(
+              product.genres.map(({name}) => getByName(name, CATEGORIES_SERVICE))
+            ),
+            platforms: await Promise.all(
+                product.operatingSystems.map((name) => 
+                getByName(name, PLATFORM_SERVICE)
+              )
+            ),
+            developers: await Promise.all(
+              product.developers.map((name) => 
+                getByName(name, DEVELOPER_SERVICE)
+              )
+            ),
+            publisher: await Promise.all(
+              product.publishers.map((name) => 
+                getByName(name, PUBLISHER_SERVICE)
+              )
+            ),
+            ...(await getGameInfo(product.slug)),//todo o retorno da função será colocada nesse objeto
+            publishedAt: new Date()
+          }
+        })
+
+        //upload image cover
+        await setImage({ image: product.coverHorizontal, game})
+        //upload image gallery
+        await Promise.all(
+          product.screenshots.slice(0, 5).map((url) => 
+            setImage({
+              image: `${url.replace(
+                "{formatter}",
+                "product_card_v2_mobile_slider_639"
+              )}`,
+              game,
+              field: "gallery"
+            })
+          )
+        )
+
+        return game
+      }
+    })
+  )
+}
+
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
     //buscando o catalogo de jogos no site Gog
     const gogApiUrl=`
 https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`
-
+    console.log("gogApiUrl: ", gogApiUrl)
     const { data: { products } } = await axios.get(gogApiUrl);
+    console.log("data: ", products)
 
     //criar todos os dados necessário para o cadastro dos jogos
-    await createManyToManyData(products)
+    // await createManyToManyData(products)
+
+    await createGames([products[0], products[1]])
   }
 }));
